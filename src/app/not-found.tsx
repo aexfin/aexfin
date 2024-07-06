610fa69cd9ae7965742a70c38585cf94ed3d59a6
aexfin <mohdsaadansari25487@gmail.com>
2024-07-06 18:22:07 +0530

    Minor Changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
             <h1 className="text-8xl">404</h1>
             <h2 className="text-neutral-400 text-2xl">Not Found</h2>
             <h4 className="text-neutral-600 text-wrap max-w-96">
-                while you're searching for your page,
+                while you are searching for your page,
             </h4>
             <h4 className="text-neutral-600 text-wrap max-w-96">
             take a sec to follow me on <a className="text-neutral-100 cursor-crosshair" href="https://github.com/aexfin">GitHub</a>
