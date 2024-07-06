726c5194cdda10080ae5ad96b059c3842fe43b5f
aexfin <mohdsaadansari25487@gmail.com>
2024-07-06 18:20:20 +0530

    Not Found Page Added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+export default function NotFound() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-8xl">404</h1>
+            <h2 className="text-neutral-400 text-2xl">Not Found</h2>
+            <h4 className="text-neutral-600 text-wrap max-w-96">
+                while you're searching for your page,
+            </h4>
+            <h4 className="text-neutral-600 text-wrap max-w-96">
+            take a sec to follow me on <a className="text-neutral-100 cursor-crosshair" href="https://github.com/aexfin">GitHub</a>
+            </h4>
+        </div>
+    )
+}
