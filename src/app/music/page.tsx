e01296b4a7eb6997d23de8bf2b2a7efc3474fddc
aexfin <mohdsaadansari25487@gmail.com>
2024-08-20 15:55:25 +0530

    Bug Fix

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -66,7 +66,7 @@ export default async function Music(res: any) {
           {tracks.map((track) => (
             <a
               href={track.external_urls.spotify}
-              id={track.id}
+              key={track.id}
               className="flex flex-col items-center justify-center bg-neutral-950 hover:bg-neutral-900 p-2"
             >
               <img
