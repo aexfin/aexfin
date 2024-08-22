65cbad0abc0ed41d4151a6b3917f0cc39c34be30
aexfin <mohdsaadansari25487@gmail.com>
2024-08-22 22:44:16 +0530

    Minor Changes

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default async function Navigation() {
     return (
-      <div className="flex flex-row w-4/5 lg:w-1/3 items-center bg-neutral-950 justify-between px-4">
+      <div className="flex flex-row w-5/6 lg:w-1/3 items-center bg-neutral-950 justify-between px-4">
         <Link
           href="/"
           className="px-1 hover:bg-violet-400 hover:text-neutral-950"
@@ -20,6 +20,12 @@ export default async function Navigation() {
           className="px-1 hover:bg-violet-400 hover:text-neutral-950"
         >
           Projects
+        </Link>
+        <Link
+          href="/music"
+          className="px-1 hover:bg-violet-400 hover:text-neutral-950"
+        >
+          Music
         </Link>
       </div>
     );
--- src/app/components/tools.tsx
+++ src/app/components/tools.tsx
@@ -1,9 +1,9 @@
 export default async function Tools() {
     return (
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="hover:text-violet-400">Languages/Tools</h1>
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h1 className=" hover:text-violet-400">Languages/Tools</h1>
         <div className="grid grid-cols-4 grid-rows-* items-center justify-center gap-2">
-          <i className="devicon-html5-plain text-2xl  p-1 bg-neutral-950 hover:bg-violet-400 hover:text-neutral-950" />
+          <i className="devicon-html5-plain text-xl p-1 bg-neutral-950 hover:bg-violet-400 hover:text-neutral-950" />
           <i className="devicon-css3-plain text-xl p-1 bg-neutral-950 hover:bg-violet-400 hover:text-neutral-950" />
           <i className="devicon-javascript-plain text-xl p-1 bg-neutral-950 hover:bg-violet-400 hover:text-neutral-950" />
           <i className="devicon-python-plain text-xl p-1 bg-neutral-950 hover:bg-violet-400 hover:text-neutral-950" />
