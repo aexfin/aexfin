c2ce0af2aa7eb2bc680a747daf6bdc8a4cb977b9
aexfin <mohdsaadansari25487@gmail.com>
2024-08-24 20:38:20 +0530

    Minor Changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,6 +5,7 @@ import Socials from "../components/socials";
 import Tools from "../components/tools";
 
 export default async function About() {
+
   return (
     <main className="flex flex-col lg:flex-row items-center justify-center gap-6 p-6">
       <Profile />
