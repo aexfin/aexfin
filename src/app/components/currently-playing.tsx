0b90dfb6b85fc0e37e01ced5034810a36340e74c
aexfin <mohdsaadansari25487@gmail.com>
2024-08-24 15:58:50 +0530

    Minor Changes

--- src/app/components/currently-playing.tsx
+++ src/app/components/currently-playing.tsx
@@ -5,7 +5,7 @@ export default async function CurrentlyPlaying() {
   return (
     <div
       className={`w-auto h-32 flex flex-row items-center justify-center bg-neutral-950 ${
-      playing ? "animate-pulse" : ""
+        playing ? "animate-pulse" : ""
       }`}
     >
       <img
@@ -13,28 +13,26 @@ export default async function CurrentlyPlaying() {
         alt={title}
         className="w-32 h-full pointer-events-none"
       />
-      <div className="flex flex-col items-center justify-center m-4">
-        <p className="text-center text-md">
+      <div className="flex flex-col items-center justify-center m-6">
+        <p className="text-center text-sm">
           {playing ? "Listening to" : "Last played"}
         </p>
         <h1
           className={`text-center ${
-          playing ? "text-green-400" : "text-neutral-300"
+            playing ? "text-green-400" : "text-neutral-300"
           }`}
         >
           {title}
         </h1>
-        <p className="text-center text-md">
+        <p className="text-center text-sm">
           by{" "}
           <span
-            className={playing ? "text-green-400" : "text-neutral-300 text-md"}
+            className={playing ? "text-green-400" : "text-neutral-300 text-sm"}
           >
             {artist}
           </span>
         </p>
-        {played_at && (
-          <h1 className="text-md">{played_at}</h1>
-        )}
+        {played_at && <h1 className="text-center text-sm">{played_at}</h1>}
       </div>
     </div>
   );
